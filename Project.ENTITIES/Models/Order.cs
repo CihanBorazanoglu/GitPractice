a5689e9ae6cda11da53f4380ca40356d20231e04
eren.bektas <eren.bektas@KDK-402-PC02-SY>
2022-08-08 14:07:14 +0300

    Tablolar oluşturuldu entegre edildi

---
+++ Project.ENTITIES/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Order:BaseEntity
+    {
+        public string Adress { get; set; }
+
+        public virtual List<ProductOrder> ProductOrders { get; set; }
+
+
+    }
+}
